3264136e464ecaa1ebdce9a206bd95eb9cfc0ec6
tdingsun <tiger.dingsun@gmail.com>
2022-07-18 13:43:56 -0700

    better header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   playlistLink = "https://www.youtube.com/playlist?list=PLsW-ZGvYnUZc1vfa84Fm1yJ2_lP4LXp_U";
   songOfTheSummer = "SONG OF THE SUMMER — 2022"
-  playlist = "☆♪☆♪☆♫☆PLAYLIST☆♫☆♪☆♪☆"
+  playlist = "☆♪☆♪☆♫ PLAYLIST ♫☆♪☆♪☆"
   content = this.songOfTheSummer
   constructor() { }
 
